cbc5f7a5ae52965a1f01a7be32f821e6b1c15cbf
Vargas1723 <vargas23.bv@gmail.com>
2022-04-27 10:52:41 -0500

    Comentariado y Resultados promedio

--- mapas.py
+++ mapas.py
@@ -4,23 +4,34 @@ import time
 
 
 
-
+# Calcular tiempo y distancia de cada ruta
+# Recibe el grafo y secuencia de nodos del camino
 def calculate(G, path):
     resultTime = 0
     resultDistance = 0
+    # Obtener los resultados de la ruta
     routes = ox.utils_graph.get_route_edge_attributes(G, path, attribute=None, retrieve_default=None)
     for route in routes:
         resultTime += route['travel_time']
         resultDistance += route['length']
     return resultTime, resultDistance
 
-
+# Imprimir los resultados de cada ruta
+# Regresa los valores para obtener el promedio
+# de cada de tiempo de ejecucion, distancia y 
+# tiempo de recorrido de cada algoritmo
 def printResults(G, path):
     time, distance = calculate(G, path)
     print('Calculate distance: ', distance)
     print('Calculate time: ', time)
     ox.plot_graph_route(G, path)
+    return time, distance
 
+# Obtiene el camino desde el nodo origen
+# al nodo destino, mediante el uso del
+# diccionario parents, que tiene cada
+# nodo y su padre, de acuerdp con la
+# busqueda de cada algoritmo
 def backtrace(parents, start, end):
     path = []
     current = end
@@ -32,7 +43,7 @@ def backtrace(parents, start, end):
         current = parents[current]
     return path
 
-
+# Algoritmo BFS
 def bfs(graph, node, dst):  # function for BFS
     visited = []  # List for visited nodes.
     queue = []  # Initialize a queue
@@ -40,11 +51,10 @@ def bfs(graph, node, dst):  # function for BFS
     visited.append(node)
     queue.append(node)
 
-    while queue:  # Creating loop to visit each node
+    while queue:  
         current = queue.pop(0)
         if current == dst:
             return True, backtrace(parent, node, dst)
-        # print (current, end = " S ")
 
         edges = list(graph.out_edges(current))
 
@@ -55,12 +65,11 @@ def bfs(graph, node, dst):  # function for BFS
                 queue.append(neighbour[1])
     return False
 
-
+# Algoritmo DFS y DLS
 def dfsSearch(graph, node, dst, depth, limit, visited, parent):
     if not limit or depth <= limit:
         depth += 1
         if (node == dst):
-            print('Econtre nodo: ', node)
             return True, parent
         if node not in visited:
             visited.append(node)
@@ -70,7 +79,7 @@ def dfsSearch(graph, node, dst, depth, limit, visited, parent):
                     parent[neighbour[1]] = node
                     if dfsSearch(graph, neighbour[1], dst, depth, limit, visited, parent): return True, parent
 
-
+# Funcion de preparacion para algoritmo DFS y DLS
 def dfs(graph, node, dst, limit=0):
     visited = []
     parent = {}
@@ -79,117 +88,137 @@ def dfs(graph, node, dst, limit=0):
         exito, parent = dfsSearch(graph, node, dst, depth, limit, visited, parent)
         if exito: return exito, backtrace(parent, node, dst)
     except:
+        # Permite incrementar el numero limite
+        # del nivel en caso de no encontrar 
+        # una ruta con el limite establecido
         return dfs(graph, node, dst, limit+10)
 
 
 def ucs(graph, start, goal, data='length'):
-    # minimum cost upto
-    # goal state from starting
+ 
     parent = {}
     answer = []
-    # create a priority queue
     queue = []
-    # set the answer vector to max value
     for i in range(len(goal)):
         answer.append(10 ** 8)
-    # insert the starting index
     queue.append([0, start])
-    # map to store visited node
     visited = {}
-    # count
     count = 0
-    # while the queue is not empty
     while (len(queue) > 0):
-        # get the top element of the
         queue = sorted(queue)
         p = queue[-1]
-        # pop the element
         del queue[-1]
-        # get the original value
         p[0] *= -1
-        # check if the element is part of
-        # the goal list
         if (p[1] in goal):
-            # get the position
             index = goal.index(p[1])
-
-            # if a new goal is reached
             if (answer[index] == 10 ** 8):
                 count += 1
-            # if the cost is less
             if (answer[index] > p[0]):
                 answer[index] = p[0]
-            # pop the element
             del queue[-1]
             queue = sorted(queue)
             if (count == len(goal)):
                 return answer, backtrace(parent, start, goal[0])
-        # check for the non visited nodes
-        # which are adjacent to present node
         if (p[1] not in visited):
-
             edges = list(graph.out_edges(p[1], data=data))
             for i in range(len(edges)):
-                # value is multiplied by -1 so that
-                # least priority is at the top
                 if(edges[i][1] not in visited):
                     parent[edges[i][1]] = edges[i][0]
                     queue.append([p[0] + edges[i][2], edges[i][1]])
-        # mark as visited
         visited[p[1]] = 1
 
     return answer, parent
 
-
-# Driver Code
+# Prueba con todos los algoritmos una pareja de nodos
 def testProblem1(Graph, start, end):
-
+    timeExec = []
+    distance = []
+    timeA = []
     # BFS
     print(" - Calculating BFS Route - ")
     start_time = time.time()
     exito, path = bfs(Graph, start, end)
     print("Tiempo empleado")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    printResults(Graph, path)
+    texe = (time.time() - start_time)
+    print("--- %s seconds ---" % texe)
+    tmp1, tmp2 = printResults(Graph, path)
+    timeExec.append(texe)
+    timeA.append(tmp1)
+    distance.append(tmp2)
     print()
     # DFS
     print(" - Calculating DFS Route - ")
     start_time = time.time()
     exito, path = dfs(Graph, start, end)
-    print("Tiempo empleado")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    printResults(Graph, path)
+    texe = (time.time() - start_time)
+    print("--- %s seconds ---" % texe)
+    tmp1, tmp2 = printResults(Graph, path)
+    timeExec.append(texe)
+    timeA.append(tmp1)
+    distance.append(tmp2)
     print()
     # DLS
     print(" - Calculating DLS Route - ")
     start_time = time.time()
     exito, path = dfs(Graph, start, end, limit=30)
     print("Tiempo empleado")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    printResults(Graph, path)
+    texe = (time.time() - start_time)
+    print("--- %s seconds ---" % texe)
+    tmp1, tmp2 = printResults(Graph, path)
+    timeExec.append(texe)
+    timeA.append(tmp1)
+    distance.append(tmp2)
     print()
     # UCS Distance
     print(" - Calculating UCS Distance Route - ")
     start_time = time.time()
     exito, path = ucs(Graph, start, [end], data='length')
     print("Tiempo empleado")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    printResults(Graph, path)
+    texe = (time.time() - start_time)
+    print("--- %s seconds ---" % texe)
+    tmp1, tmp2 = printResults(Graph, path)
+    timeExec.append(texe)
+    timeA.append(tmp1)
+    distance.append(tmp2)
     print()
     # UCS Time
     print(" - Calculating UCS Time Route - ")
     start_time = time.time()
     exito, path = ucs(Graph, start, [end], data='travel_time')
     print("Tiempo empleado")
-    print("--- %s seconds ---" % (time.time() - start_time))
-    printResults(Graph, path)
+    texe = (time.time() - start_time)
+    print("--- %s seconds ---" % texe)
+    tmp1, tmp2 = printResults(Graph, path)
+    timeExec.append(texe)
+    timeA.append(tmp1)
+    distance.append(tmp2)
     print()
+    return timeExec, timeA, distance
 
+# Imprime los resultados promedio de toda la ejecucion
+def printAverageResult(algoritm, texe, time, dist):
+    print("Average Results ", algoritm, " : Ratio 1km")
+    print("Time execution: ", texe / 10)
+    print("Time : ", time / 10)
+    print("Distance: ", dist / 10)
 
+# Ejecucion de las 10 parejas de puntos
 def runProblem1(Graph, locations):
+    algoritmNames = ['BFS', 'DFS', 'DLS', 'UCS Distance', 'UCS Time']
+    timeExecA = [0, 0, 0, 0, 0]
+    distanceA = [0, 0, 0, 0, 0]
+    timeA = [0, 0, 0, 0, 0]
     for i in range(len(locations)):
         print('Pareja de putnos #', i+1)
-        testProblem1(Graph, locations[i][0], locations[i][1])
+        tmp1, tmp2, tmp3 = testProblem1(Graph, locations[i][0], locations[i][1])
+        for j in range(5):
+
+            timeExecA[j] = timeExecA[j] + tmp1[j]
+            distanceA[j] = distanceA[j] + tmp2[j]
+            timeA[j] = timeA[j] + tmp3[j]
+    for i in range(5):
+        printAverageResult(algoritmNames[i], timeExecA[i], distanceA[i], timeA[i])
+
 
 # Data
 def main():
